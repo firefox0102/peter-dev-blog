b8a6de6f965d09f5d23624e9bdc72723c01350a8
Peter Finn <firefox0102@gmail.com>
2023-05-18 12:11:06 -0700

    Style tweaks

--- src/components/ExperienceGroupLeftAdornment.tsx
+++ src/components/ExperienceGroupLeftAdornment.tsx
@@ -3,7 +3,7 @@ import type { FunctionComponent } from "react"
 export const ExperienceGroupLeftAdornment: FunctionComponent = () => {
   return (
     <div
-      className="mx-5 border border-skin-line"
+      className="mr-5 border border-skin-line"
       style={{ borderWidth: "1px" }}
     ></div>
   )
